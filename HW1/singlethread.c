c69b8716fad0632453d84ad6dec15f86733569ac
emilycup <eemly.le@gmail.com>
2016-01-15 23:50:28 -0800

    added comments for better understanding

--- HW1/singlethread.c
+++ HW1/singlethread.c
@@ -1,9 +1,16 @@
+// ==== Team Members =======================================================
+//	* Daniel
+//	* Emily Le
+//	* Max Wolotsky
+// ========================================================================
+
 #include <stdio.h>
 #include <stdlib.h>
 #define N 100
 #define TRUE 1
 #define FALSE 0
 
+// prototypes
 void producer();
 void consumer();
 int produce_item();
@@ -14,18 +21,29 @@ void consume_item(int);
 int count = 0;
 int items_array[N];
 
+// produce a random integer that will be added to an array
 int produce_item() {
 	return rand();	
 }
+
+// will put generated random integer into array
 void insert_item(int item) {
 	items_array[count] = item;
 }
+
+// will remove integer at iteration and will return the value
 int remove_item() {
 	return items_array[count];
 }
 void consume_item(int item) {
 	printf("%d\n",item);
 }
+
+// ==== Producer  ===========================================================
+//	If the count reaches N the function will stop running to let the
+//	other function (consumer) run. Otherwise the count will continue to
+//	increment.
+// ==========================================================================
 void producer() {
 	int item;
 	while (TRUE) {
@@ -36,6 +54,11 @@ void producer() {
 	}
 }
 
+// === Consumer ============================================================
+//	If the Count reaches 0, the function will stop running and allow the
+//	other function (Producer) to run. Otherwise the counter will continue
+//	to decrement.
+// =========================================================================
 void consumer() {
 	int item;
 	while (TRUE) {
@@ -46,6 +69,10 @@ void consumer() {
 	}
 }
 
+// === main =================================================================
+//	The $PC will ooint to this functional initially to start the program.
+//	It continuously calls the Producer and Consumer functions forever.	
+// ==========================================================================
 int main() {
 	//items_array = (int*) malloc(N);
 	while (TRUE) {
