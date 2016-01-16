40b3b84448b006964f3ad3eb7e5d06351312f705
Max Wolotsky <mwolotsky@gmail.com>
2016-01-16 11:22:43 -0800

    Outline for MultiThread with Semaphore

---
+++ HW1/multi_sem.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#define N 100
+#define TRUE 1
+#define FALSE 0
+
+typedef int semaphore;
+semaphore mutex = 1;
+semaphore empty = N;
+semaphore full = 0;
+
+void producer(void);
+void consumer(void);
+
+void producer(void) {
+	int item;
+	
+	while (TRUE) {
+		item = produce_item();
+		down(&empty);
+		down(&mutex);
+		insert_item(item);
+		up(&mutex);
+		up(&full);
+	}
+}
+void consumer(void) {
+	int item;
+
+	while (TRUE) {
+		down(&full);
+		down(&mutex);
+		item = remove_item();
+		up(&mutex);
+		up(&empty);
+		consume_item(item);
+	}
+}
+
