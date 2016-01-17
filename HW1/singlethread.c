d784d10607628155b01d1c34ad61d4dcfd495a78
Max Wolotsky <mwolotsky@gmail.com>
2016-01-16 16:46:58 -0800

    Fixed Single Threaded Code

--- HW1/singlethread.c
+++ HW1/singlethread.c
@@ -4,11 +4,11 @@
 #define TRUE 1
 #define FALSE 0
 
-void producer();
-void consumer();
-int produce_item();
+void producer(void);
+void consumer(void);
+int produce_item(void);
 void insert_item(int);
-int remove_item();
+int remove_item(void);
 void consume_item(int);
 
 int count = 0;
@@ -17,22 +17,26 @@ int items_array[N];
 int produce_item() {
 	return rand();	
 }
+
 void insert_item(int item) {
-	items_array[count] = item;
+	items_array[count++] = item;
 }
+
 int remove_item() {
-	return items_array[count];
+	return items_array[--count];
 }
+
 void consume_item(int item) {
-	printf("%d\n",item);
+	printf("Consumed: %d\n",item);
 }
+
 void producer() {
 	int item;
 	while (TRUE) {
 		item = produce_item();
 		if (count == N) return;
 		insert_item(item);
-		count = count + 1;
+		printf("Produced: %d\n",item);
 	}
 }
 
@@ -40,14 +44,12 @@ void consumer() {
 	int item;
 	while (TRUE) {
 		if (count == 0) return;
-		count = count - 1;
 		item = remove_item();
 		consume_item(item);
 	}
 }
 
 int main() {
-	//items_array = (int*) malloc(N);
 	while (TRUE) {
 		producer();
 		consumer();
