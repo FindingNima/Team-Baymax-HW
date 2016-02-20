14109d043ccd9699517653c228bd23efc5f25a98
Max Wolotsky <mwolotsky@gmail.com>
2016-02-19 17:30:14 -0800

    Actually Fixed Thread Stuff

--- HW2/semaphore_hw2.c
+++ HW2/semaphore_hw2.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <fcntl.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -65,6 +66,12 @@ int main(int argc, char const *argv[])
 	num_of_buffers = atoi(argv[3]);		
 	num_of_items = atoi(argv[4]);
 
+	// allocate threads
+	prod_id = (pthread_t*) malloc(num_of_producers * sizeof(pthread_t*));
+	cons_id = (pthread_t*) malloc(num_of_consumers * sizeof(pthread_t*));
+	char *prod_num[num_of_producers];
+	char *cons_num[num_of_consumers];
+
 	// create an array of buffers
 	int buffers[num_of_buffers];
 	sem_t buffer_index_sem[num_of_buffers];
@@ -81,7 +88,19 @@ int main(int argc, char const *argv[])
 	// whole buffer list
 	sem_init(&buffer_sem, 0, 1);
 
+	//Create producer threads
+	for (i = 0; i < num_of_producers; i++)
+	{
+		sprintf(&prod_num[i],"%d",i);
+		pthread_create(&prod_id[i],NULL,(void*)&producer,&prod_num[i]);
+	}
 	
+	//Create consumer threads
+	for (i = 0; i < num_of_consumers; i++)
+	{
+		sprintf(&cons_num[i],"%d",i);
+		pthread_create(&cons_id[i],NULL,(void*)&consumer,&cons_num[i]);
+	}
 
 
 	return 0;
