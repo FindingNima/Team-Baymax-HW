1571e6ec7954815532f152e579280309764b0409
Max Wolotsky <mwolotsky@gmail.com>
2016-02-19 17:27:11 -0800

    Added Thread Creation

---
+++ HW2/hw.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#define N 1024
+#define TRUE 1
+#define FALSE 0
+
+int main(int argc, char** argv) {
+	
+	return 0;	
+}
